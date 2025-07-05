2133673c9a103ad8a7a38c6eed42a16e06d41d87
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-05 15:31:11 +0200

    Fix text colored based on accuracy

--- wpmTest/main.py
+++ wpmTest/main.py
@@ -13,7 +13,12 @@ def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
 
     for i, char in enumerate(current):
-        stdscr.addstr(0, i, char, curses.color_pair(1))
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, i, char, color)
 
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this app!"
@@ -37,7 +42,7 @@ def wpm_test(stdscr):
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
     wpm_test(stdscr)
