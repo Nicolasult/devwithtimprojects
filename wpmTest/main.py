6c3e813a478be5675abadaa3de800edb398c63b8
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-05 15:16:27 +0200

    add wpmtest function

--- wpmTest/main.py
+++ wpmTest/main.py
@@ -9,11 +9,28 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+
+    while True:
+        key = stdscr.getkey()
+        current_text.append(key)
+
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr.refresh()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
