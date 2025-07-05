cb45247cf0944895cac94ef925a5fa2d3f519988
nicolasult <nicolas.ultrillo@gmail.com>
2025-07-05 15:08:32 +0200

    Add start screen function

--- wpmTest/main.py
+++ wpmTest/main.py
@@ -1,14 +1,19 @@
 import curses
 from curses import wrapper
 
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Welcom the to Speed Tpying Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    stdscr.addstr(1, 0, "Hello world!")
-    stdscr.refresh()
-    stdscr.getkey()
+    start_screen(stdscr)
 
 wrapper(main)
